d96cefcb1b474611b2c956ecd3725c5c610abd99
Braden Ganetsky <braden.ganetsky@gmail.com>
2026-08-10 00:35:08 -0500

    Refinement of automatic typescript

--- _build.sh
+++ _build.sh
@@ -4,11 +4,14 @@
 while [ -n "$1" ]; do
     case "$1" in
         js)
-            shopt -s globstar
-            npx esbuild **/*.tsx --outbase=. --outdir=.
+            shopt -s globstar \
+                && npx esbuild **/*.ts --bundle --outbase=. --outdir=/_js \
+                && npx esbuild **/*.tsx --bundle --outbase=. --outdir=/_js \
+            || exit 1
             ;;
         *)
             echo "Unknown argument: $1"
+            exit 1
             ;;
     esac
     shift
